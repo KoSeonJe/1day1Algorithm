f23a6b3d0e14fe8333e24ae2c4e703c8103d97df
koseonje <127813439+KoSeonJe@users.noreply.github.com>
2024-08-09 21:42:17 +0900

    [Bronze IV] Title: 주사위 세개, Time: 140 ms, Memory: 14568 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,34 +1,55 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.List;
 
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        
-        if(a==b & b==c){
-            System.out.println(10000+(a*1000));
-        }else if(a==b | b==c | a==c){
-            if(a==b){
-                System.out.println(1000+(a*100));
-            }else if(b==c){
-                System.out.println(1000+(b*100));
-                
-            }else if(a==c){
-                System.out.println(1000+(a*100));
-            }
-        }else if(a!=b& a!=c & b!=c){
-            if(a>b&a>c){
-            System.out.println(a*100);                
-            }else if(b>a&b>c){
-            System.out.println(b*100);                
-            }else if(c>a&c>b){
-            System.out.println(c*100);                
-            }
+
+public class Main {
+
+    private static int solution(int[] nums) {
+      int answer = 0;
+      List<Integer> list = new ArrayList<>(List.of(nums[0], nums[1], nums[2]));
+      List<Integer> dislist = list.stream().distinct().collect(Collectors.toList());
+      if (dislist.size() == 3) {
+        int max = Collections.max(dislist);
+        return max * 100;
+      } else if (dislist.size() == 2) {
+        if(list.get(0) == list.get(1)) {
+          return 1000 + list.get(0) * 100;
+        } else if (list.get(1) == list.get(2)) {
+          return 1000 + list.get(1) * 100;
+        } else if (list.get(2) == list.get(0)) {
+          return 1000 + list.get(2) * 100;
         }
-        
-        sc.close();
+      }
+      return 10000 + dislist.get(0) * 1000;
     }
-}
+
+    public static void main(String[] args) throws IOException {
+      int[] input = getInput();
+      resolve(solution(input));
+    }
+
+    private static int[] getInput() throws IOException {
+      int[] input;
+      try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+        int[] nums = Arrays.stream(br.readLine().split(" "))
+            .mapToInt(Integer::parseInt)
+            .toArray();
+        input = nums;
+      }
+      return input;
+    }
+
+    private static void resolve(int answer) throws IOException {
+      try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+        bw.write(String.valueOf(answer));
+      }
+    }
+  }
