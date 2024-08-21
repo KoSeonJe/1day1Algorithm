d77626b5a3d0243c7e8e105a5ec843b32b616246
koseonje <127813439+KoSeonJe@users.noreply.github.com>
2024-08-22 01:49:10 +0900

    [Bronze II] Title: 수 정렬하기, Time: 120 ms, Memory: 14856 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,29 +1,39 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int n = Integer.parseInt(br.readLine());
-    ArrayList<Integer> datas = inputData(n, br);
-    Collections.sort(datas);
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    for (int data : datas) {
-      System.out.println(data);
+    int n = Integer.parseInt(br.readLine());
+    List<Integer> datas = inputData(n, br);
+    Set<Integer> temp = new HashSet<>(datas);
+    List<Integer> result = new ArrayList<>(temp);
+    Collections.sort(result);
+
+    for (int data : result) {
+      bw.write(String.valueOf(data) + "\n");
     }
+
     br.close();
+    bw.close();
   }
 
-  private static ArrayList<Integer> inputData(int n, BufferedReader br) throws IOException {
-    ArrayList<Integer> temp = new ArrayList<>();
-
-    for (int i = 0; i < n ; i++) {
-      temp.add(Integer.parseInt(br.readLine()));
+  private static List<Integer> inputData(int n, BufferedReader br) throws IOException {
+    List<Integer> data = new ArrayList<>();
+    for (int i = 0; i < n; i++) {
+      data.add(Integer.parseInt(br.readLine()));
     }
-    return temp;
+    return data;
   }
 }
