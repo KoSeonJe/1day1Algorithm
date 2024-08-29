dca9b23a5720041309930174bc743b5451e928a4
koseonje <127813439+KoSeonJe@users.noreply.github.com>
2024-08-30 02:55:59 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1328 ms, Memory: 129648 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    
+    int n = Integer.parseInt(br.readLine());
+    List<Integer> datas = inputData(n, br);
+    Collections.sort(datas);
+    StringBuilder sb = new StringBuilder();
+    for (int data : datas) {
+      sb.append(data).append("\n");
+    }
+    bw.write(sb.toString());
+    br.close();
+    bw.close();
+  }
+
+  private static List<Integer> inputData(int n, BufferedReader br) throws IOException {
+    List<Integer> data = new ArrayList<>();
+    for (int i = 0; i < n; i++) {
+      data.add(Integer.valueOf(br.readLine()));
+    }
+    return data;
+  }
+}
