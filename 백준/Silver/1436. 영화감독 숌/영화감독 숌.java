4c1bf7a541b4eac30997dd39fe95c995a7b9db22
koseonje <127813439+KoSeonJe@users.noreply.github.com>
2024-09-04 11:25:20 +0900

    [Silver V] Title: 영화감독 숌, Time: 300 ms, Memory: 140348 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int n = Integer.parseInt(br.readLine());
+    int result = getTitle(n);
+    bw.write(String.valueOf(result));
+
+    br.close();
+    bw.close();
+  }
+
+  private static int getTitle(int n) {
+    int count = 0;
+    int temp = 666;
+    String sTemp = String.valueOf(temp);
+    while (count < n) {
+      sTemp = String.valueOf(temp);
+      if (sTemp.contains("666")) {
+        count++;
+      }
+      temp++;
+    }
+    return Integer.parseInt(sTemp);
+  }
+}
