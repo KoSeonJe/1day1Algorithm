2ed18816f6b104b439d4a4f4ed66ab95660979c1
koseonje <127813439+KoSeonJe@users.noreply.github.com>
2025-01-02 22:25:13 +0900

    [Silver IV] Title: 동전 0, Time: 208 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Main {
+	static Scanner scanner = new Scanner(System.in);
+	static int count =0;
+	static int rest =0;
+	static int n=0;
+	static int coin =0;
+	static int[] coinvalue;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		made();
+		System.out.println(rest);
+		
+	}
+	
+	public static void made() {
+	     n = scanner.nextInt();
+		coin = scanner.nextInt();
+		coinvalue = new int[n];
+		for(int i=coinvalue.length-1; i>=0; i--) {
+			coinvalue[i] = scanner.nextInt();
+		}
+		madehighcoin();
+		
+	}
+	public static void madehighcoin() {
+		int container=0;
+		
+		for(int highcoin : coinvalue) {
+			if(coin-highcoin >= 0) {
+				container=highcoin;
+				break;
+			}
+		}
+		coinrest(container);
+	}
+
+	public static void coinrest(int highcoin) {
+
+		
+		
+		rest = rest+coin/highcoin;
+		coin = coin%highcoin;
+		if(coin!=0) {
+			madehighcoin();
+		}
+		
+		
+		
+	}
+	
+}
