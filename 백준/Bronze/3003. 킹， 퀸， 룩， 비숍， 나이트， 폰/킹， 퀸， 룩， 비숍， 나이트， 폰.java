427870bcc67369ed0f34c29f3656f7561f348f42
koseonje <kosunje1344@naver.com>
2025-05-12 14:29:01 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 104 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -1,22 +1,40 @@
-import java.util.Scanner;
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-      
-        int a1 = sc.nextInt();
-        int a2 = sc.nextInt();
-        int a3 = sc.nextInt();
-        int a4 = sc.nextInt();
-        int a5 = sc.nextInt();
-        int a6 = sc.nextInt();
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
-        System.out.println(1-a1);
-        System.out.println(1-a2);
-        System.out.println(2-a3);
-        System.out.println(2-a4);
-        System.out.println(2-a5);
-        System.out.println(8-a6);
+public class Main {
 
-        sc.close();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<Integer> chessCount = List.of(1, 1, 2, 2, 2, 8);
+        List<Integer> input = inputChess(br);
+
+        List<Integer> results = calculateDiff(chessCount, input);
+        StringBuilder builder = new StringBuilder();
+        for (int result : results) {
+            builder.append(result).append(" ");
+        }
+        System.out.println(builder);
+        
+        br.close();
+    }
+
+    private static List<Integer> calculateDiff(List<Integer> chessCount, List<Integer> input) {
+        List<Integer> temp = new ArrayList<>();
+        for (int i = 0; i < chessCount.size(); i++) {
+            temp.add(chessCount.get(i) - input.get(i));
+        }
+        return temp;
+    }
+
+    private static List<Integer> inputChess(BufferedReader br) throws IOException {
+        String[] integers = br.readLine().split(" ");
+        List<Integer> temp = new ArrayList<>();
+        for (String integer : integers) {
+            temp.add(Integer.parseInt(integer));
+        }
+        return temp;
     }
 }
