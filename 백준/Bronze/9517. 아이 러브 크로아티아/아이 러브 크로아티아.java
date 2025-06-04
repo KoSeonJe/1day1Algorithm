3a14c5500916318e71e8efe112ab424fe284dc99
koseonje <kosunje1344@naver.com>
2025-06-04 14:34:57 +0900

    [Bronze III] Title: 아이 러브 크로아티아, Time: 100 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Bronze/9517. 아이 러브 크로아티아/아이 러브 크로아티아.java
+++ 백준/Bronze/9517. 아이 러브 크로아티아/아이 러브 크로아티아.java
@@ -1,31 +1,65 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.Objects;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int K = Integer.parseInt(br.readLine());
-		int N = Integer.parseInt(br.readLine());
-		//지나간 시간을 저장 할 변수
-		int time = 0;
-		
-		for(int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int T = Integer.parseInt(st.nextToken());
-			String Z = st.nextToken();
-			
-			time += T;
-			
-			if(time > 210) break;
-			
-			if(Z.equals("T")) K++;
-			
-			if(K == 9) K = 1;
-		}
-		System.out.println(K);
-	}
 
+    static int finish = 210;
+
+    static class Info {
+        int time;
+        String answerType;
+
+        public Info(int time, String answerType) {
+            this.time = time;
+            this.answerType = answerType;
+        }
+
+        public int getTime() {
+            return time;
+        }
+
+        public String getAnswerType() {
+            return answerType;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int k = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        List<Info> infos = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String[] part = br.readLine().split(" ");
+            infos.add(new Info(Integer.parseInt(part[0]), part[1]));
+        }
+
+        Deque<Integer> people = new ArrayDeque<>();
+        for (int i = 0; i < 8; i++) {
+            int temp = (k - 1 + i) % 8 + 1;
+            people.add(temp);
+        }
+
+        int tempTime = 0;
+        for (Info info : infos) {
+            tempTime += info.getTime();
+            if (finish <= tempTime) {
+                break;
+            }
+            if (Objects.equals(info.answerType, "T")) {
+                int pass = people.removeFirst();
+                people.add(pass);
+            }
+        }
+        int realResult = people.getFirst();
+        System.out.println(realResult);
+
+        br.close();
+    }
 }
